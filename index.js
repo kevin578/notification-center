ca4cb17fc3c7b5109223b0823d82f616fdfbac01
kevin578 <kevinbriggs1@gmail.com>
2018-12-08 17:30:11 -0500

    routing for index.html

--- index.js
+++ index.js
@@ -57,6 +57,11 @@ app.post("/api/deleteHistory", (req, res)=> {
     })
 })
 
+const path = require("path");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
+
 // app.post('/api/addDevice', (req, res) => {
 //     const Device = new deviceModel({token: req.body.token});
 //     Device.save(()=> {
