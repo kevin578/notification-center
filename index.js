d32022d1b4e86976de58834878ce0b8992317217
kevin578 <kevinbriggs1@gmail.com>
2018-12-08 17:41:10 -0500

    added whole function

--- index.js
+++ index.js
@@ -57,10 +57,18 @@ app.post("/api/deleteHistory", (req, res)=> {
     })
 })
 
-const path = require("path");
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static("client/build"));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
 
 // app.post('/api/addDevice', (req, res) => {
 //     const Device = new deviceModel({token: req.body.token});
